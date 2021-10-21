e09dad79b020a1aab51f55105a6f4c47e3b2617c
Aimer <lovv840629y@gmail.com>
2021-10-21 15:55:00 +0800

    chore: update eslint

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Edit <code>src/App.js</code> and save to reload. </p>
+        <p>
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
